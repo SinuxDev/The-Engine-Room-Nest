163054324b78d34bbda9f519ccedc8e88b2e566a
Aung Ye Htet <aung.atg1738@gmail.com>
2025-12-07 20:39:32 +0630

    feat: checkout and commit msg rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,24 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // New feature
+        'fix', // Bug fix
+        'docs', // Documentation only
+        'style', // Formatting, missing semicolons, etc
+        'refactor', // Code change that neither fixes a bug nor adds a feature
+        'perf', // Performance improvement
+        'test', // Adding tests
+        'chore', // Maintain
+        'revert', // Revert to a commit
+        'ci', // CI related changes
+      ],
+    ],
+    'subject-case': [0], // Allow any case in subject
+    'subject-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 100],
+  },
+};
