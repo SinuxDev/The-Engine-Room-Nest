17b17c1855b443b58015518ff9515d9a8a867b5e
Aung Ye Htet <aung.atg1738@gmail.com>
2025-12-07 18:50:10 +0630

    feat: config for the error message return

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const messages = errors.map((error) => ({
+          error: Object.values(error.constraints || {}).join(', '),
+        }));
+
+        return new BadRequestException({
+          message: messages,
+        });
+      },
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
