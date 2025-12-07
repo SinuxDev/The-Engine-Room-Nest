8575ee6f4f76133b0ea0a42f18dd578d4de6f6c2
Aung Ye Htet <aung.atg1738@gmail.com>
2025-12-07 20:12:21 +0630

    feat: some gurad

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -8,16 +8,20 @@ import {
   UsePipes,
   ValidationPipe,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from '../dtos/CreateUser.dto';
 import { UsersService } from '../services/users/users.service';
 import { ValidateCreateUserPipe } from '../pipes/validate-create-user/validate-create-user.pipe';
+import { AuthGuard } from '../guards/auth/auth.guard';
 
 @Controller('users')
+@UseGuards(AuthGuard)
 export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   getAllUsers(@Query('name') name?: string) {
     return this.userService.getAllUsers(name);
   }
